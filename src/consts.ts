ab63441380b1d757a013ee223c525b64c0764b7e
teluis <teluis5@example.com>
2026-09-09 00:27:13 +0900

    Pivot from estate to finance

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-﻿export const SITE_TITLE = 'teluis finance';
-export const SITE_DESCRIPTION = 'Financial news and insights.';
+﻿export const SITE_TITLE = 'Finance & Investment';
+export const SITE_DESCRIPTION = 'マクロ経済、株式、債券、そして不動産まで、あらゆる資産運用と金融知識を論理的に解説します。';
